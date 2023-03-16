de864d07df4d9908c2e8b9da8396160baaa6a47d
morigene <orisoft52@gmail.com>
2023-03-16 18:44:56 +0000

    adding * to comment first beginning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- *  main and put syntax
- *  Return : Always 0(success)
+
+/**
+ * main - Entry point
+ * puts
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-puts("\" Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
