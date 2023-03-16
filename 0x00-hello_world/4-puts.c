5fd2f9741a3f8cb9cfcf5603d22ed5f03c95cb2a
morigene <orisoft52@gmail.com>
2023-03-16 18:38:45 +0000

    fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
+/*
+ *  main and put syntax
+ *  Return : Always 0(success)
+ */
+int main(void)
+{
 puts("\" Programming is like building a multilingual puzzle");
 return (0);
 }
