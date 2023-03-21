4dd7c61ba9e5bcf6bba6e6bc11533fa6345f1d44
morigene <orisoft52@gmail.com>
2023-03-21 15:12:50 +0000

    creating all source file at once

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar();
+}
+
+void _putchar(){
+
+    puts("_putchar");
+}
