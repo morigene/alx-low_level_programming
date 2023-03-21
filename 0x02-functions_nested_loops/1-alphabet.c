84f84604b769a7416f89ddae5fccd5238f171b3d
morigene <orisoft52@gmail.com>
2023-03-21 18:42:41 +0000

    print alphabet and printing 10 times of alphat letters for task 1 and 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Description - prints all alphabet, in lowercase, followed by a new line
+ * Return
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+}
