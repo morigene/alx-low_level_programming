0f9a15ac3cd98d94cc892e8d101fc4a1cbcac3c2
morigene <orisoft52@gmail.com>
2023-03-22 13:22:39 +0000

    adding time table, and fibonnocci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point for executing program
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int sum;
+
+	i = 0;
+	n = 1024;
+	sum = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+		i++;
+	}
+		printf("%d\n", sum);
+
+	return (0);
+}
