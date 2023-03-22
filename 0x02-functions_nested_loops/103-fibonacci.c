a1e2380f9eeb834721e81507c9ce36a7105ac497
morigene <orisoft52@gmail.com>
2023-03-22 16:30:03 +0000

    printing  50 first and fobonicss even number not over 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main - Prints the add of the even-valued
+ * fibonacci numbers.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	long int n1, n2, sum, sumeven;
 
+	n1 = 1;
+	n2 = 2;
+	sum = sumeven = 0;
+	while (fn <= 4000000)
+	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		if ((n1 % 2) == 0)
+		{
+			sumeven += n1;
+		}
+	}
+	printf("%ld\n", sumeven);
 	return (0);
 }
