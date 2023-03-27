6ffb1c11d4f8d751a8dad40a090c1b25261efc9c
morigene <orisoft52@gmail.com>
2023-03-27 14:22:11 +0000

    fix identation

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,23 +1,21 @@
 #include "main.h"
-
 /**
- *_puts - print a string followed by new line
- *@str: input string
- *Return: no return
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
  */
-
 void _puts(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (i >=0 )
+	while (count >= 0)
 	{
-		if (str[i]  == '\0')
-		_putchar('\n');
-	         break;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
-
-	_putchar('\n')
-	i++;
-
 }
